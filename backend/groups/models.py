85bf73168656f7724161e65f961e56683b858038
mmj-sys <mmj.sys@gmail.com>
2022-06-27 17:48:02 +0430

    collection model created

--- backend/groups/models.py
+++ backend/groups/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class collection(models.Model):
+    collection_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='collection_images/', blank=True)
+    color = models.CharField(max_length=100, default='#ffffff')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
